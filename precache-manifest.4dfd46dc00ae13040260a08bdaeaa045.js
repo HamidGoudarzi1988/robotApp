fb63b70883fba0498b3195c4ac8bdb27f3adfe71
https://github.com/HamidGoudarzi1988/Profile <goudarzi.hamid@gmail.com>
2020-04-14 15:05:59 -0400

    Updates

---
+++ precache-manifest.4dfd46dc00ae13040260a08bdaeaa045.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e80b899feea5b4c5155f2d7a200bf5",
+    "url": "/robotApp/index.html"
+  },
+  {
+    "revision": "784cb488839b968a62c2",
+    "url": "/robotApp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dc232e569fd8d86a988e",
+    "url": "/robotApp/static/css/main.e2a1f800.chunk.css"
+  },
+  {
+    "revision": "784cb488839b968a62c2",
+    "url": "/robotApp/static/js/2.84d6e4e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotApp/static/js/2.84d6e4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc232e569fd8d86a988e",
+    "url": "/robotApp/static/js/main.4b68c64b.chunk.js"
+  },
+  {
+    "revision": "3186b0583114e1062d11",
+    "url": "/robotApp/static/js/runtime-main.1c866e30.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robotApp/static/media/SEGA.82e09f78.TTF"
+  }
+]);
